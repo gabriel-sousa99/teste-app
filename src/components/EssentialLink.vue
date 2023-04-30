8021514d1f2335d520b4c171f2df16605e1baf04
Gabriel Sousa <gabriel.sousa@unimedvr.com.br>
2023-04-30 18:49:48 -0300

    mudando a cor pra amarelo

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,14 +1,6 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
@@ -19,31 +11,37 @@
   </q-item>
 </template>
 
+<style>
+body {
+  background-color: goldenrod;
+}
+</style>
+
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'EssentialLink',
+  name: "EssentialLink",
   props: {
     title: {
       type: String,
-      required: true
+      required: true,
     },
 
     caption: {
       type: String,
-      default: ''
+      default: "",
     },
 
     link: {
       type: String,
-      default: '#'
+      default: "#",
     },
 
     icon: {
       type: String,
-      default: ''
-    }
-  }
-})
+      default: "",
+    },
+  },
+});
 </script>
